934e72d13347800a3d95e7b2382d4d4af4fd06ed
Jeffrey Rivas <xxjeffrey2006xx@gmail.com>
2025-04-28 19:48:18 -0600

    Determinante recursivo GOD

--- data.js
+++ data.js
@@ -452,6 +452,123 @@ function trans(){
     }
 }
 
+function det(){ // Hay un problema a la hora de hacer un nuevo cálculo
+    const back=document.getElementById('back');
+    document.body.style.overflow='hidden';
+    back.innerHTML='';
+
+    const inputs=document.querySelectorAll('#det .box_1 textarea');
+    const rows=parseInt(inputs[0].value);
+    const cols=parseInt(inputs[1].value);
+
+    if(isNaN(rows) || isNaN(cols) || rows<=0 || cols<=0){
+        alert('Por favor, ingrese números válidos para filas y columnas.');
+        return;
+    }
+
+    if(rows!==cols){
+        alert('La matriz debe ser cuadrada.');
+        return;
+    }
+
+    const container=document.createElement('div');
+    container.style.display='flex';
+    container.style.flexDirection='column';
+    container.style.alignItems='center';
+    container.style.padding='2rem';
+    container.style.backgroundColor='white';
+    container.style.margin='5rem auto';
+    container.style.width='fit-content';
+
+    let matrixA=[];
+    const tableA=create_table(rows, cols, 'matrixA');
+    tableA.style.marginTop='1rem';
+    const calc_button=document.createElement('button');
+    calc_button.innerText='Calcular';
+    calc_button.className='button';
+    calc_button.onclick=() => calculate_det();
+
+    container.appendChild(document.createElement('h2')).innerText='Matriz A';
+    container.appendChild(tableA);
+    container.appendChild(calc_button);
+
+    back.appendChild(container);
+    back.style.display='flex';
+    back.style.justifyContent='center';
+    back.style.alignItems='center';
+
+    function calculate_det(){
+        matrixA=read_matrix(tableA);
+        const matrix_aux=read_matrix(tableA);
+
+        if(!matrixA){
+            alert('Ningún espacio puede quedar vacío.');
+            return;
+        }
+
+        document.body.style.overflow='auto';
+        back.style.display='none';
+
+        det = determinante(matrixA);
+
+        const result=document.getElementById('det');
+        const original_res=result.innerHTML;
+        result.innerHTML='';
+
+        result.innerHTML=`<div style="font-size: 1.5rem; text-align: center;">
+            \\[ A = ${matrix_latex(matrixA).slice(2, -2)} \\]
+            \\[ \\det(A) = ${det} \\]
+        </div>`;
+
+        const restart_button=document.createElement('button');
+        restart_button.innerText='Nuevo cálculo';
+        restart_button.className='button';
+        restart_button.onclick=() => {
+            result.classList.remove('active');
+            setTimeout(() => {
+                result.innerHTML=original_res;
+                result.classList.add('active');
+            }, 400);
+        }
+
+        result.appendChild(restart_button);
+        MathJax.typeset();
+
+        function determinante(A){
+            const tam =  A.length;
+            let det = 0;
+            
+            if (tam == 1){
+                det = A[0][0];
+            } else {
+                for (let j = 0; j < tam; j++){
+                    det = det + (A[0][j] * cofactor(A, 0, j));
+                }
+            }
+        
+            return det;
+        }
+
+        function cofactor(A, fila, columna){
+            const tam = A.length;
+            const submatriz = [];
+            for (let i = 0; i < tam; i++){
+                if (i !== fila){
+                    const fila_aux = [];
+                    for (let j = 0; j < tam; j++){
+                        if (j !== columna){
+                            fila_aux.push(A[i][j]);
+                        }
+                    }
+                    submatriz.push(fila_aux);
+                }
+            }
+        
+            return Math.pow(-1, fila + columna) * determinante(submatriz);
+        }
+    }
+}
+
 function inverse(){
     const back=document.getElementById('back');
     document.body.style.overflow='hidden';
